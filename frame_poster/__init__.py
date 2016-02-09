724f7e82028c361aa468cc600e820f0d72464296
Matt Black <github@mafro.net>
2016-02-09 12:13:46 +0000

    initial commit

---
+++ frame_poster/__init__.py
@@ -0,0 +1,10 @@
+#
+# frame-poster
+#
+
+__title__ = 'frame-poster'
+__version__ = '0.0.1'
+__build__ = 0x000001
+__author__ = 'Matt Black'
+__license__ = 'Simplified BSD License'
+__copyright__ = 'Copyright 2016 Matt Black'
